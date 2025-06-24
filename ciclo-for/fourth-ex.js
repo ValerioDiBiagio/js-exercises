5caa0ec66e986eb74b13dfc00a5cc4cae61e15c1
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-06-24 11:16:12 +0200

    Implement for loop to calculate the sum of integers from 1 to user-defined number

--- ciclo-for/fourth-ex.js
+++ ciclo-for/fourth-ex.js
@@ -1,1 +1,11 @@
 // chiedere all'utente di inserire un numero e poi calcolare la somma di tutti i numeri da 1 a N
+
+const num = parseInt(prompt('Inserisci un numero:'));
+
+let sum = 0;
+
+for (let i = 1; i <= num; i++) {
+    sum += i;
+}
+
+console.log(sum);
--- ciclo-for/second-ex.js
+++ ciclo-for/second-ex.js
@@ -1,7 +1,7 @@
 // Scrivi un programma che utilizzi un ciclo for per calcolare la somma di numeri interi da 1 a 100 stampando il risultato nella console
 
-let sum = 0;
-for (let i = 1; i <= 100; i++) {
-    sum += i;
-}
-console.log(sum);
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//     sum += i;
+// }
+// console.log(sum);
