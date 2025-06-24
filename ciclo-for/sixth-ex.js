b9c1a247a209db00c3edfb874afff5525bf94cc9
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-06-24 11:33:12 +0200

    Add functionality to print multiplication table of user-defined number up to 10

--- ciclo-for/sixth-ex.js
+++ ciclo-for/sixth-ex.js
@@ -1,1 +1,11 @@
 // Chiedere all'utente un numero e stampare la sua tabellina (fino a 10) nella console.
+
+const n = parseInt(prompt('inserisci un numero'))
+
+let mult = 0;
+
+for (let i = 1; i <= 10; i++) {
+    mult = n * i;
+    console.log(mult)
+}
+
