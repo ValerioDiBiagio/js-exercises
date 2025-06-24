9d4fc8e6ab5a61617a06afe85fbde58fe7fee4d8
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-06-24 12:25:00 +0200

    Add second exercise JavaScript file for fruit array manipulation

---
+++ array/second-ex.js
@@ -0,0 +1,19 @@
+// Partendo dall'array frutti dell'esercizio precedente.
+// Aggiungi un nuovo frutto alla fine dell'array. Stampa l'array.
+// Aggiungi un nuovo frutto all'inizio dell'array. Stampa l'array.
+// Rimuovi l'ultimo elemento dall'array. Stampa l'array.
+// Rimuovi il primo elemento dall'array. Stampa l'array.
+
+const fruttiDue = ["mela", "anguria", "arancia", "banana", "fragola"];
+
+fruttiDue.push("pera");
+console.log(fruttiDue);
+
+fruttiDue.unshift("lampone")
+console.log(fruttiDue)
+
+fruttiDue.pop()
+console.log(fruttiDue)
+
+fruttiDue.shift()
+console.log(fruttiDue)
