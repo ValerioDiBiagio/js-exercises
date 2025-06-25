d277a84315fd9117c8ab144c6851d0fa8415561a
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-06-25 12:42:20 +0200

    Add fourteenth exercise for array manipulation and update index.html to include it

--- array/thirteenth-ex.js
+++ array/thirteenth-ex.js
@@ -1,8 +1,8 @@
 // Crea un array di nomi nomi = ["Alice", "Bob", "Charlie"].
 // Utilizza il metodo forEach() per stampare ogni nome sulla console, preceduto dalla frase "Ciao, ".
 
-const nomi = ["Alice", "Bob", "Charlie"];
+// const nomi = ["Alice", "Bob", "Charlie"];
 
-nomi.forEach(function (nome) {
-    console.log(`ciao ${nome}`)
-})
+// nomi.forEach(function (nome) {
+//     console.log(`ciao ${nome}`)
+// })
