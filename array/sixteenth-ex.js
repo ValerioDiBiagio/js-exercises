67e822f8506456e40ba84a9795d0f31216bac3bf
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-06-25 13:08:35 +0200

    Add sixteenth exercise for array manipulation and update index.html to include it

---
+++ array/sixteenth-ex.js
@@ -0,0 +1,23 @@
+// Hai un array di numeri che rappresentano i punteggi ottenuti in un test: punteggi = [85, 92, 78, 65, 95, 72].
+
+// Utilizza il metodo forEach() per:
+
+// Stampare ogni punteggio, indicando se è un punteggio "alto" (maggiore o uguale a 80) o "basso" (minore di 80).
+// Calcolare la media di tutti i punteggi.
+// Stampa la media finale alla fine.
+
+const punteggi = [85, 92, 78, 65, 95, 72];
+
+let sum = 0;
+
+punteggi.forEach(punteggio => {
+    if (punteggio > 80) {
+        console.log(`il punteggio è ${punteggio} quindi è alto!`)
+    } else {
+        console.log(`il punteggio è ${punteggio} quindi è basso!`)
+    }
+
+    sum += punteggio
+})
+
+console.log(sum / punteggi.length)
