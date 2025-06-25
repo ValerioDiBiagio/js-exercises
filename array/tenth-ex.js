4c0bf1b1611b59dc00fe44df8f9318acfe8ed55a
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-06-25 11:55:25 +0200

    Add eleventh exercise for array manipulation and include it in index.html

--- array/tenth-ex.js
+++ array/tenth-ex.js
@@ -1,8 +1,8 @@
-// Hai un array di numeri numeri = [1, 2, 3, 4, 5]. 
+// Hai un array di numeri numeri = [1, 2, 3, 4, 5].
 // Utilizza il metodo map() per creare un nuovo array in cui ogni numero sia moltiplicato per 2. Stampa il nuovo array.
 
 
-const numeri = [1, 2, 3, 4, 5];
+// const numeri = [1, 2, 3, 4, 5];
 
-const numeriMolt = numeri.map(numero => numero * 2);
-console.log(numeriMolt)
+// const numeriMolt = numeri.map(numero => numero * 2);
+// console.log(numeriMolt)
