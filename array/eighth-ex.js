d5eb4490e15ec415bf9cec7ef0f330b0a3aa9986
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-06-25 11:32:42 +0200

    Add ninth exercise for array manipulation and include it in index.html

--- array/eighth-ex.js
+++ array/eighth-ex.js
@@ -1,13 +1,13 @@
-// Dato l'array prodotti = ["computer", "mouse", "tastiera", "monitor"], scrivi del codice che verifichi se la stringa "tastiera" è presente nell'array. 
+// Dato l'array prodotti = ["computer", "mouse", "tastiera", "monitor"], scrivi del codice che verifichi se la stringa "tastiera" è presente nell'array.
 // Stampa un messaggio appropriato che indichi se l'elemento è stato trovato o meno.
 
-const prodotti = ["computer", "mouse", "tastiera", "monitor"];
+// const prodotti = ["computer", "mouse", "tastiera", "monitor"];
 
-const prodottoCercato = "tastiera";
+// const prodottoCercato = "tastiera";
 
-if (prodotti.includes(prodottoCercato)) {
-    console.log(`l'elemento ${prodottoCercato} è presente nell'array`);
+// if (prodotti.includes(prodottoCercato)) {
+//     console.log(`l'elemento ${prodottoCercato} è presente nell'array`);
 
-} else {
-    console.log(`l'elemento ${prodottoCercato} non è presente nell'array`);
-}
+// } else {
+//     console.log(`l'elemento ${prodottoCercato} non è presente nell'array`);
+// }
---
+++ array/ninth-ex.js
@@ -0,0 +1,6 @@
+// Dato l'array città = ["Roma", "Milano", "Napoli"], cambia il secondo elemento (Milano) in "Firenze". Stampa l'array prima e dopo la modifica.
+
+const citta = ["Roma", "Milano", "Napoli"];
+
+citta[1] = "Firenze";
+console.log(citta);
