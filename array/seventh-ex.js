41900e141fa8c7ed70d0edd92a92f2bcd92a85df
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-06-25 11:12:03 +0200

    Uncomment seventh exercise script in index.html and add seventh exercise for array manipulation

---
+++ array/seventh-ex.js
@@ -0,0 +1,8 @@
+// Crea un array chiamato colori con i seguenti elementi: "rosso", "verde", "blu".
+// Utilizza un ciclo for per stampare ogni elemento dell'array sulla console.
+
+const colori = ["rosso", "verde", "blu"];
+
+for (let i = 0; i < colori.length; i++) {
+    console.log(colori[i]);
+} 
--- array/sixth-ex.js
+++ array/sixth-ex.js
@@ -5,16 +5,16 @@
 // Rimuovi l'ultimo elemento dall'array.
 // Rimuovi il primo elemento dall'array. Stampa l'array dopo ogni operazione per vedere le modifiche
 
-const numeri = [1, 2, 3];
+// const numeri = [1, 2, 3];
 
-numeri.push(4);
-console.log(numeri);
+// numeri.push(4);
+// console.log(numeri);
 
-numeri.unshift(0);
-console.log(numeri);
+// numeri.unshift(0);
+// console.log(numeri);
 
-numeri.pop()
-console.log(numeri);
+// numeri.pop()
+// console.log(numeri);
 
-numeri.shift()
-console.log(numeri);
+// numeri.shift()
+// console.log(numeri);
