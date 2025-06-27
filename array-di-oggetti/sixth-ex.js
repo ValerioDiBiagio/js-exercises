2cfc5df956e94df6dcb6696d6d2784d595585e4f
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-06-27 11:38:58 +0200

    Add first exercise for greeting users and update index.html to include it

--- array-di-oggetti/sixth-ex.js
+++ array-di-oggetti/sixth-ex.js
@@ -4,48 +4,48 @@
 
 // Cerca un utente per nome (es. "Mario"). Se lo trovi, stampa tutte le sue informazioni. Se non lo trovi, stampa un messaggio appropriato.
 
-const utenti = [
-    {
-        id: 1,
-        nome: "Marco",
-        eta: 15
-    },
-    {
-        id: 2,
-        nome: "Luca",
-        eta: 18
-    },
-    {
-        id: 3,
-        nome: "Mattia",
-        eta: 20
-    },
-    {
-        id: 4,
-        nome: "Stefano",
-        eta: 17
-    },
-    {
-        id: 5,
-        nome: "Franco",
-        eta: 22
-    }
-]
+// const utenti = [
+//     {
+//         id: 1,
+//         nome: "Marco",
+//         eta: 15
+//     },
+//     {
+//         id: 2,
+//         nome: "Luca",
+//         eta: 18
+//     },
+//     {
+//         id: 3,
+//         nome: "Mattia",
+//         eta: 20
+//     },
+//     {
+//         id: 4,
+//         nome: "Stefano",
+//         eta: 17
+//     },
+//     {
+//         id: 5,
+//         nome: "Franco",
+//         eta: 22
+//     }
+// ]
 
-const utenteDue = utenti.find(utente => utente.id === 2);
-console.log(utenteDue)
+// const utenteDue = utenti.find(utente => utente.id === 2);
+// console.log(utenteDue)
 
-utenteDue.eta = 30;
-console.log(utenteDue)
+// utenteDue.eta = 30;
+// console.log(utenteDue)
 
 
-const nomeUtente = "Franco"
-const utenteCercato = utenti.find(utente => utente.nome === nomeUtente)
+// const nomeUtente = "Franco"
+// const utenteCercato = utenti.find(utente => utente.nome === nomeUtente)
 
-if (utenteCercato) {
-    console.log(`l'utente ${nomeUtente} è stato trovato`, utenteCercato)
-} else {
-    console.log(`l'utente ${nomeUtente} non è stato trovato`)
-}
+// if (utenteCercato) {
+//     console.log(`l'utente ${nomeUtente} è stato trovato`, utenteCercato)
+// } else {
+//     console.log(`l'utente ${nomeUtente} non è stato trovato`)
+// }
 
 
---
+++ funzioni/first-ex.js
@@ -0,0 +1,11 @@
+// Crea una funzione chiamata salutaUtente che non accetta nessun parametro.
+
+// All'interno della funzione, stampa in console il messaggio: "Ciao! Benvenuto/a nel mondo delle funzioni."
+
+// Chiama la funzione salutaUtente.
+
+function salutaUtente() {
+    console.log("Ciao! Benvenuto/a nel mondo delle funzioni.")
+}
+
+salutaUtente()
