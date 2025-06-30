61890590c40a95d62e7e69fc84931a335a4e6ae5
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-06-30 10:29:01 +0200

    Add eighth exercise for finding the maximum of two numbers and update index.html to include it

---
+++ funzioni/eighth-ex.js
@@ -0,0 +1,13 @@
+// Crea una funzione chiamata trovaMassimo che prende due numeri come argomenti e restituisce il numero più grande.
+
+function trovaMassimo(num1, num2) {
+    if (num1 > num2) {
+        return num1
+
+    } else {
+        return num2
+    }
+}
+
+
+console.log(trovaMassimo(5, 8));
--- funzioni/seventh-ex.js
+++ funzioni/seventh-ex.js
@@ -1,7 +1,7 @@
 // Crea una funzione chiamata isPari che prende un numero come argomento e restituisce true se il numero è pari, altrimenti false.
 
-function isPari(num) {
-    return num % 2 === 0
-}
+// function isPari(num) {
+//     return num % 2 === 0
+// }
 
-console.log(isPari(8))
+// console.log(isPari(8))
