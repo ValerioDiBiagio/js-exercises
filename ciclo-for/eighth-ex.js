8fd202b355faa769034d57d1840b81d8106f3450
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-04 12:08:10 +0200

    Add eighth exercise for finding even numbers and summing them, and update index.html to include it

---
+++ ciclo-for/eighth-ex.js
@@ -0,0 +1,11 @@
+// Usa un ciclo for per trovare tutti i numeri pari tra 1 e 20.
+// Somma tutti quei numeri.
+// Alla fine, stampa la somma totale.
+
+let sum = 0;
+
+for (let i = 0; i <= 20; i += 2) {
+    sum += i
+}
+
+console.log(sum)
