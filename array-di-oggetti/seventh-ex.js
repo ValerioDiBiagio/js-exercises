5f9e9273e02a06bfd95f1f0a011475cc422cf344
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-04 13:00:30 +0200

    Add sixth exercise for modifying a person object and update index.html to include it

--- array-di-oggetti/seventh-ex.js
+++ array-di-oggetti/seventh-ex.js
@@ -10,30 +10,30 @@
 // Calcola il prezzo totale di questi prodotti.
 // Stampa il totale con un messaggio
 
-const prodotti = [{
-    nome: "Pc",
-    prezzo: 1245,
-    disponibile: true
-},
-{
-    nome: "Penna",
-    prezzo: 1,
-    disponibile: true
-},
-{
-    nome: "Monitor",
-    prezzo: 245,
-    disponibile: false
-},
-{
-    nome: "Libro",
-    prezzo: 12,
-    disponibile: true
-},
-]
+// const prodotti = [{
+//     nome: "Pc",
+//     prezzo: 1245,
+//     disponibile: true
+// },
+// {
+//     nome: "Penna",
+//     prezzo: 1,
+//     disponibile: true
+// },
+// {
+//     nome: "Monitor",
+//     prezzo: 245,
+//     disponibile: false
+// },
+// {
+//     nome: "Libro",
+//     prezzo: 12,
+//     disponibile: true
+// },
+// ]
 
-let prodDisp = prodotti.filter(prodotto => prodotto.disponibile === true)
-console.log(prodDisp);
+// let prodDisp = prodotti.filter(prodotto => prodotto.disponibile === true)
+// console.log(prodDisp);
 
-const prezzoTot = prodDisp.reduce((totale, prodotti) => totale + prodotti.prezzo, 0);
-console.log(`il prezzo totale dei prodotti disponibili è: ${prezzoTot}`);
+// const prezzoTot = prodDisp.reduce((totale, prodotti) => totale + prodotti.prezzo, 0);
+// console.log(`il prezzo totale dei prodotti disponibili è: ${prezzoTot}`);
---
+++ oggetti/sixth-ex.js
@@ -0,0 +1,32 @@
+// Modifica un oggetto persona
+// Hai un oggetto persona con le seguenti proprietà:
+
+// nome 
+
+// età 
+
+// occupazione 
+
+// Cosa devi fare:
+
+// Stampa un messaggio con il nome e l'età,
+
+// Cambia il valore dell'occupazione in "Sviluppatore".
+
+// Aggiungi una nuova proprietà chiamata hobby con valore "lettura".
+
+// Stampa l’oggetto aggiornato.
+
+const persona = {
+    nome: "Luca",
+    eta: 23,
+    occupazione: "Studente"
+}
+
+console.log(`Mi chiamo ${persona.nome} e ho ${persona.eta} anni`);
+
+persona.occupazione = "Sviluppatore";
+
+persona.hobby = "Lettura";
+
+console.log(persona);
